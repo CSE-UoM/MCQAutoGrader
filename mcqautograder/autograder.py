863debdec86eca790f491dce3e6d12cc001ac205
Gayashan Amarasinghe <gayashan.amarasinghe@gmail.com>
2023-01-04 10:06:14 +0530

    Moving configurations to a file

--- mcqautograder/autograder.py
+++ mcqautograder/autograder.py
@@ -11,16 +11,17 @@ import argparse
 import csv
 import time
 from statistics import mean
+import json
 
 
 FINAL_MARK = 'mark'
 FILE_NAME = 'file_name'
 
 
-def read_image(path):
+def read_image(path, config):
     img = Image.open(path).convert('L')
     enhancer = ImageEnhance.Contrast(img)
-    img = enhancer.enhance(11)
+    img = enhancer.enhance(config["image_processing"]["enhance_contrast"])
     color = ImageEnhance.Color(img)
     img = color.enhance(0.0)
     img.resize((1000, 1000))
@@ -59,20 +60,22 @@ def get_homography(img1, img2):
     return H
 
 
-def get_coordinates_of_bubbles():
+def get_coordinates_of_bubbles(config):
     # y = x/105 + 143
-    x = 390  # starting x coordinate of the first bubble
-    y = 510  # starting y coordinate of the first bubble
-    x_offset = 111
-    y_offset = 92.3
-    x_column_offset = 780
+    # starting x coordinate of the first bubble
+    x = config['bubble_coordinates']['starting_x']
+    # starting y coordinate of the first bubble
+    y = config['bubble_coordinates']['starting_y']
+    x_offset = config['bubble_coordinates']['x_offset']
+    y_offset = config['bubble_coordinates']['y_offset']
+    x_column_offset = config['bubble_coordinates']['x_column_offset']
     coordinates = []
     for i in range(3):  # for every column
         if i == 1:
-            y = 515
+            y = config['bubble_coordinates']['columns']['2']['starting_y']
             # y_offset = 92.3
         if i == 2:
-            y = 520
+            y = config['bubble_coordinates']['columns']['3']['starting_y']
             # y_offset = 92.3
         for j in range(30):  # for every question
             for k in range(5):  # for every choice
@@ -83,14 +86,14 @@ def get_coordinates_of_bubbles():
                 # y_i = (x_i/105)+143
                 a = [x_i, y_i]
                 coordinates.append(a)
-            x = x - 1.8
+            x = x - config['bubble_coordinates']['x_adjustment']
             # y_offset *= 0.9985
         # y_offset = 94
         # if i==1:
             # x_column_offset = 790
         x += x_column_offset
     # The number of choices of each question in a sequential manner
-    choice_distribution = [5 for i in range(90)]
+    choice_distribution = [5 for i in range(config['num_questions'])]
     # coordinates.append([240, 145])  # Top left
     # coordinates.append([2340, 165]) # Top right
     # coordinates.append([175, 3260]) # Bottom left
@@ -169,33 +172,37 @@ def get_answers(img1, img2, points, is_marking_scheme, show_intermediate_results
     return answer
 
 
-def calculate_score(marking_scheme, answer_script, choice_distribution):
+def calculate_score(marking_scheme, answer_script, choice_distribution, facility_index=None):
     idd = 0
     choice_distribution = np.array(choice_distribution)
     incorrect = []
     correct = []
     more_than_one_marked = []
     not_marked = []
+    columnwise_total = {0: 0, 1: 0, 2: 0}
     correct_mark = 0
     for i in range(0, choice_distribution.shape[0]):    # for every question
         correct_choice = False
         marked_choices_per_question = 0
         for k in range(0, choice_distribution[i]):  # for every choice
-            if marking_scheme[idd] == 1 and marking_scheme[idd] == answer_script[idd]:
+            if marking_scheme[idd] == 1 and answer_script[idd] == 1:
                 correct_choice = True
-            if answer_script[idd] == 1:
+            if answer_script[idd] == 1:  # count the number of marked choices
                 marked_choices_per_question += 1
             idd += 1
         if correct_choice and marked_choices_per_question == 1:
             correct.append(i+1)
             correct_mark += 1
+            columnwise_total[i//30] += 1
+            if facility_index:
+                facility_index[i+1] += 1
         elif marked_choices_per_question > 1:
             more_than_one_marked.append(i+1)
         elif marked_choices_per_question == 0:
             not_marked.append(i+1)
         else:
             incorrect.append(i+1)
-    return correct, incorrect, more_than_one_marked, not_marked
+    return correct, incorrect, more_than_one_marked, not_marked, columnwise_total
 
 
 def plot_marked_answer_sheet(marking_scheme, answer_script, template_img, bubble_coordinates, file_name='output.pdf', show_plot=False, save_plot=False):
@@ -227,56 +234,93 @@ def plot_marked_answer_sheet(marking_scheme, answer_script, template_img, bubble
     plt.close()
 
 
+def print_to_stdout_and_file(file_handler, text, display_index):
+    if display_index:
+        print(text)
+    file_handler.write(text + '\n')
+
+
+def get_facility_index(version, facility_index_file_name, num_students, facility_index, config, display_index=False):
+    difficult_questions = []
+    easy_questions = []
+    easy_threshold = config['outputs']['facility_index']['easy_threshold']
+    difficult_threshold = config['outputs']['facility_index']['difficult_threshold']
+    with open(facility_index_file_name, 'w') as facility_index_file:
+        heading = f"Facility index for {version}:"
+        print_to_stdout_and_file(facility_index_file, heading, display_index)
+
+        for k, v in facility_index.items():
+            index = round(v*100/num_students, 2)
+            if index < difficult_threshold:
+                difficult_questions.append((k, index))
+            if index > easy_threshold:
+                easy_questions.append((k, index))
+            question_stat = f"    Q{k}: {index}%"
+            print_to_stdout_and_file(
+                facility_index_file, question_stat, display_index)
+        difficult_questions_heading = f"Difficult questions (<{difficult_threshold}%):"
+        print_to_stdout_and_file(
+            facility_index_file, difficult_questions_heading, display_index)
+        for q in difficult_questions:
+            diff_q_stat = f'    Q{q[0]} ({q[1]}%)'
+            print_to_stdout_and_file(
+                facility_index_file, diff_q_stat, display_index)
+        easy_questions_heading = f"Easy questions (>{easy_threshold}%):"
+        print_to_stdout_and_file(
+            facility_index_file, easy_questions_heading, display_index)
+        for q in easy_questions:
+            easy_q_stat = f'    Q{q[0]} ({q[1]}%)'
+            print_to_stdout_and_file(
+                facility_index_file, easy_q_stat, display_index)
+
+
 def app():
     start = time.time()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--verbose", help="Print detailed messages",
-                        action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument("--markingscheme", help="Path to the bubble sheet of the marking scheme",
-                        default="/home/gayashan/projects/cse/2022/MCQAutoGrader/samples/marking_schemes/1.jpg")
-    parser.add_argument(
-        "--template", help="Path to the template of the bubble sheet", default="/home/gayashan/projects/cse/cs1033/templates/1.jpg")
-    parser.add_argument(
-        "--answers", help="Path to the directory containing scanned answer scripts", default="samples/answers3/")
-    parser.add_argument(
-        "--debug", help="Show intermediate results and debug messages", action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument(
-        "--showmarked", help="Show the answer script marked with correct and incorrect answers", action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument(
-        "--savemarked", help="Save the answer script marked with correct and incorrect answers", action=argparse.BooleanOptionalAction, default=True)
-    parser.add_argument(
-        "--studentslist", help="Name of the file containing a list of the students' index numbers in csv format", default="samples/students_list.csv")
-    parser.add_argument(
-        "--output", help="Directory to save the output files such as the output.csv file containing the list of students and respective marks in csv format", default="output/")
-    parser.add_argument(
-        "--outputcsv", help="Name of the output file containing the list of students and respective marks in csv format", default="output.csv")
-    parser.add_argument(
-        "--ignoreinputcsv", help="Ignore the input list of students given as a CSV file and use the file name instead", action=argparse.BooleanOptionalAction, default=True)
+    print("Running autograder...")
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", help="Print detailed messages", action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("--debug", help="Show intermediate results and debug messages", action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("--showmarked", help="Show the answer script marked with correct and incorrect answers", action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("--savemarked", help="Save the answer script marked with correct and incorrect answers", action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument("--ignoreinputcsv", help="Ignore the input list of students given as a CSV file and use the file name instead", action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("--showfacilityindex", help="Show the facility index on the screen", action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("--config", help="Configuration file for the particular version", default="configurations/v2.json")
+    parser.add_argument("--markingscheme", help="Path to the bubble sheet of the marking scheme", default="samples/marking_schemes/2.jpg")
+    parser.add_argument("--template", help="Path to the template of the bubble sheet", default="samples/templates/2.jpg")
+    parser.add_argument("--answers", help="Path to the directory containing scanned answer scripts", default="samples/answers_v2_test/")
+    parser.add_argument("--studentslist", help="Name of the file containing a list of the students' index numbers in csv format", default="samples/students_list.csv")
+    parser.add_argument("--output", help="Directory to save the output files such as the output.csv file containing the list of students and respective marks in csv format", default="output/")
+    parser.add_argument("--outputcsv", help="Name of the output file containing the list of students and respective marks in csv format", default="output.csv")
+    parser.add_argument("--version", help="Version of the exam paper", default="v1")
     args = parser.parse_args()
 
-    print("Running autograder...")
-    if not os.path.exists(args.output):
-        os.makedirs(args.output)
+    version = args.version
+    debug = args.debug
+    verbose = args.verbose
+    output_dir = args.output
+    config_file = args.config
+    answers_dir = args.answers
+    students_list_file = args.studentslist
+    output_csv_file = args.outputcsv
+    ignore_input_csv = args.ignoreinputcsv
+    template_file = args.template
+    marking_scheme_file = args.markingscheme
+    show_facility_index = args.showfacilityindex
+    show_marked_answer = args.showmarked
+    save_marked_answer = args.savemarked
 
-    output_file_name = os.path.join(args.output, args.outputcsv)
-    with open(output_file_name, 'w') as output_file:
-        writer = csv.writer(output_file)
-        writer.writerow(['Index No', 'Autograded Final Mark', 'More than one choice marked', 'No choices marked', 'Answer Script'])
-    
-    template_img = read_image(args.template)
-    marking_scheme_img = read_image(args.markingscheme)
-    student_marks = dict()
-    bubble_coordinates, choice_distribution = get_coordinates_of_bubbles()
-    marking_scheme = get_answers(
-        template_img, marking_scheme_img, bubble_coordinates, is_marking_scheme=True, show_intermediate_results=args.debug)
-    per_answer_time_list = []
-    i = 0
-    answer_script_files_list = sorted(glob.glob(args.answers + '*.jpg'))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    if not args.ignoreinputcsv:
+    with open(config_file, 'r') as file:
+        print(f"Reading configurations from {config_file}")
+        config = json.load(file)
+
+    answer_script_files_list = sorted(glob.glob(answers_dir + '*.jpg'))
+    if not ignore_input_csv:
         try:
-            with open(args.studentslist, mode='r') as students_csv:
+            with open(students_list_file, mode='r') as students_csv:
                 csv_reader = csv.DictReader(students_csv)
                 students = [row['Index No'] for row in csv_reader]
         except FileNotFoundError as e:
@@ -287,45 +331,76 @@ def app():
 
     if len(answer_script_files_list) != len(students):
         print(
-            f"Mismatch in the number of entries in the provided {args.studentslist} csv file and the number of scanned scripts in the {args.answers} directory.")
-        print(f"Entries in the csv file: {len(students)}")
+            f"Mismatch in the number of entries in the provided {students_list_file} csv file and the number of scanned scripts in the {answers_dir} directory.")
+        print(f"Entries in the {students_list_file} file: {len(students)}")
         print(
-            f"Entries in the scanned directory: {len(answer_script_files_list)}")
+            f"Entries in the {answers_dir} directory: {len(answer_script_files_list)}")
         sys.exit()
+
+    output_file_name = os.path.join(output_dir, output_csv_file)
+    facility_index_file_name = os.path.join(output_dir, f"facility_index_{version}.txt")
+    with open(output_file_name, 'w') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(['Index No', 'Autograded Final Mark',
+                        'More than one choice marked', 'No choices marked', 'Column 1 Total', 'Column 2 Total', 'Column 3 Total', 'Answer Script'])
+
+    template_img = read_image(template_file, config)
+    marking_scheme_img = read_image(marking_scheme_file, config)
+    student_marks = dict()
+    bubble_coordinates, choice_distribution = get_coordinates_of_bubbles(
+        config)
+    marking_scheme = get_answers(
+        template_img, marking_scheme_img, bubble_coordinates, is_marking_scheme=True, show_intermediate_results=debug)
+    per_answer_time_list = []
+    i = 0
+
+    # initiate facult index calculation
+    facility_index = {}
+    for l in range(1, len(choice_distribution)+1):
+        facility_index[l] = 0
 
     with open(output_file_name, 'a') as output_file:
         writer = csv.writer(output_file)
 
         for answer_script_file_path in answer_script_files_list:
             start_per_answer = time.time()
-            answer_script_img = read_image(answer_script_file_path)
+            answer_script_img = read_image(answer_script_file_path, config)
 
-            answer_script = get_answers(template_img, answer_script_img,
-                                        bubble_coordinates, is_marking_scheme=False, show_intermediate_results=args.debug)
+            try:
+                answer_script = get_answers(template_img, answer_script_img,
+                                        bubble_coordinates, is_marking_scheme=False, show_intermediate_results=debug)
+            except IndexError as e:
+                print("###################")
+                print(f"Error in grading {students[i]}. Please manually grade. Autograded mark is recorded as 0 for this student.: ", e)
+                print("###################")
+                writer.writerow([students[i], 0, 'Not autograded', 'Not autograded', 0, 0, 0, answer_script_file_path])
+                i += 1
+                end_per_answer = time.time()
+                per_answer_time_list.append(end_per_answer - start_per_answer)
+                continue
 
-            correct, incorrect, more_than_one_marked, not_marked = calculate_score(
-                marking_scheme, answer_script, choice_distribution)
-            if args.verbose:
+            correct, incorrect, more_than_one_marked, not_marked, columnwise_total = calculate_score(
+                marking_scheme, answer_script, choice_distribution, facility_index=facility_index)
+            if verbose:
                 print(
                     f"Our observation for the first {len(choice_distribution)} questions are :")
                 print("Correct answers are:", correct)
                 print("Incorrect or Unresponded answers are:", incorrect)
                 print("More than one choice is marked:", more_than_one_marked)
                 print("No choices were marked:", not_marked)
-            if args.showmarked or args.savemarked:
-                marked_file_name = os.path.join(
-                    args.output, f"{students[i]}_output.png")
+            if show_marked_answer or save_marked_answer:
+                marked_file_name = os.path.join(output_dir, f"{students[i]}_output.png")
                 plot_marked_answer_sheet(
-                    marking_scheme, answer_script, template_img, bubble_coordinates, file_name=marked_file_name, show_plot=args.showmarked, save_plot=args.savemarked)
-            
+                    marking_scheme, answer_script, template_img, bubble_coordinates, file_name=marked_file_name, show_plot=show_marked_answer, save_plot=save_marked_answer)
+
             final_mark = len(correct)
             print(
                 f"{i+1} Result for {students[i]}: {final_mark}/90, Incorrect: {len(incorrect)}, More than one: {len(more_than_one_marked)}, Not marked: {len(not_marked)}")
 
-            
-            student_marks[students[i]] = {FINAL_MARK: final_mark, FILE_NAME: answer_script_file_path}
+            # student_marks[students[i]] = {FINAL_MARK: final_mark, FILE_NAME: answer_script_file_path}
             # append the row to the output csv file
-            writer.writerow([students[i], final_mark, more_than_one_marked, not_marked, answer_script_file_path])
+            writer.writerow(
+                [students[i], final_mark, more_than_one_marked, not_marked, columnwise_total[0], columnwise_total[1], columnwise_total[2], answer_script_file_path])
             i += 1
             end_per_answer = time.time()
             per_answer_time_list.append(end_per_answer - start_per_answer)
@@ -337,6 +412,9 @@ def app():
     print(f"    Total elapsed time: {total_elapsed_time}s")
     average_time_elapsed_per_paper = round(mean(per_answer_time_list), 2)
     print(f"    Average time per paper: {average_time_elapsed_per_paper}s")
+
+    get_facility_index(version, facility_index_file_name,
+                       len(answer_script_files_list), facility_index, config, display_index=show_facility_index)
 
 
 if __name__ == "__main__":
